4897224feaec83a104b7f5153c137f67a64660ad
karafitronster <karafitronster@gmail.com>
2024-11-06 14:16:11 +0700

    spawn banknote

--- Assets/Scripts/MoveBanknote.cs
+++ Assets/Scripts/MoveBanknote.cs
@@ -6,12 +6,15 @@ public class MoveBanknote : MonoBehaviour
 {
     [SerializeField] protected float delay = 0.1f;
     [SerializeField] protected float euler = 30f;
-    protected Rigidbody rb = default;
-    protected float speed = 0.5f;
+    [SerializeField] protected GameObject topFrontier = default;
+    [SerializeField] protected GameObject bottomFrontier = default;
+
+    protected float speed = 0.4f;
 
     protected virtual void OnEnable()
     {
-        //rb = GetComponent<Rigidbody>();
+        topFrontier = GameObject.Find("TopFrontier");
+        bottomFrontier = GameObject.Find("BottomFrontier");
         StartCoroutine(StartMove());
         StartCoroutine(StartRotation());
         StartCoroutine(DestroyObject());
@@ -20,19 +23,14 @@ public class MoveBanknote : MonoBehaviour
     private IEnumerator StartMove()
     {
         Vector3 currentPosition = transform.position;
-        Vector3 direction = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(0.5f, 1f), 0);
-        //direction.Normalize();
+        Vector3 direction = new Vector3(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(0.9f, 1f), 0);
         Debug.Log(direction);
         Debug.Log(Screen.height / 3f);
-        while (isActiveAndEnabled && transform.position.y < Screen.height * 2f / 3f) 
+        while (isActiveAndEnabled && transform.position.y < topFrontier.transform.position.y) 
         {
             Debug.Log(direction);
             transform.position += direction * speed;
-            //Debug.Log(Vector3.Distance(currentPosition, transform.position));
             yield return new WaitForSecondsRealtime(delay);
-            //transform.position = Vector3.Lerp(transform.position, currentPosition + direction * Screen.height/3f, 0.01f);
-
-            //if ((Vector3.Distance(currentPosition, transform.position) > Screen.height / 3f)) break;
         }
 
     }
@@ -53,7 +51,7 @@ public class MoveBanknote : MonoBehaviour
         {
             Debug.Log(transform.position.y);
             Debug.Log("destroy");
-            if(transform.position.y < -1 * Screen.height / 2f)
+            if(transform.position.y < bottomFrontier.transform.position.y)
             {
                 Destroy(gameObject);
             }
